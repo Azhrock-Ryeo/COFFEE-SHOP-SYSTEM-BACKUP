8220d7981a5c7b7d0d815e0fc9ecd57deb504396
aldrsze <aldrinjariel0@gmail.com>
2026-05-06 00:26:46 +0800

    implement CRUD product management routes and controller functions

--- api/index.js
+++ api/index.js
@@ -5,44 +5,12 @@ const db = require('./config/db');
 const app = express();
 const PORT = 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// test route
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
-
-// get products from MySQL
-app.get('/products', (req, res) => {
-  db.query('SELECT * FROM products', (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json(err);
-    }
-    res.json(result);
-  });
-});
-
-// add product to MySQL
-app.post('/products', (req, res) => {
-  const { name, description, price, stock, category_id, created_by } = req.body;
-
-  // sql query
-  const sql = `INSERT INTO products (name, description, price, stock, category_id, created_by)
-              VALUES (?, ?, ?, ?, ?, ?)`;
-
-  db.query(sql, [name, description, price, stock, category_id, created_by], (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json(err);
-    }
-    res.json({
-      message: "Success!",
-      new_product_id: result.insertId,
-    });
-  });
-});
+// route
+app.use('/products', require('./routes/productRoutes'))
 
 // start server
 app.listen(PORT, () => {
