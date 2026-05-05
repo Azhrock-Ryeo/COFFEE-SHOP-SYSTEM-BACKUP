db73fd64721334f4966db34f8e618bd9147f9700
aldrsze <aldrinjariel0@gmail.com>
2026-05-05 22:49:23 +0800

    add product

--- api/index.js
+++ api/index.js
@@ -24,6 +24,27 @@ app.get('/products', (req, res) => {
   });
 });
 
+// add product to MySQL
+app.post('/products', (req, res) => {
+  const { name, description, price, stock, category_id, created_by } = req.body;
+
+  // sql query
+  const sql = `INSERT INTO products (name, description, price, stock, category_id, created_by)
+              VALUES (?, ?, ?, ?, ?, ?)`;
+
+  db.query(sql, [name, description, price, stock, category_id, created_by], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+    res.json({
+      message: "Success!",
+      new_product_id: result.insertId,
+    });
+  });
+});
+
+// start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
